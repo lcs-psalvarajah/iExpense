207507d1a4afd1d170650577d133b8114a470cb6
jina <psalvarajah@lcs.on.ca>
2020-11-24 14:52:45 -0500

    This creates the function where users can create rows and then delete them when they are not wanted

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,29 +7,28 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
-    var name: String
-    
-    var body: some View {
-        Button("Dismiss") {
-            self.presentationMode.wrappedValue.dismiss()
-        }
-    }
-}
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button ("Show sheet") {
-            self.showingSheet.toggle()
-            
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "jina")
-            
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("\($0)")
+                }
+                .onDelete(perform: removeRows)
+            }
+            Button("Add Number"){
+                self.numbers.append(self.currentNumber)
+                self.currentNumber += 1
+            }
         }
 
+    }
+    
+    func removeRows (at offsets: IndexSet) {
+        numbers.remove (atOffsets: offsets)
     }
 }
 

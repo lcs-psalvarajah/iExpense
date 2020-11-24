13e94642a0ec3b405535f0344cfe9d501e2598ee
jina <psalvarajah@lcs.on.ca>
2020-11-24 14:42:52 -0500

    this shows and hides a second view by single button click.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,20 +7,29 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbio"
-    @Published var lastName = "Baggins"
-}
-struct ContentView: View {
-    
-    @ObservedObject private var user = User()
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var name: String
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Dismiss") {
+            self.presentationMode.wrappedValue.dismiss()
         }
+    }
+}
+struct ContentView: View {
+    @State private var showingSheet = false
+    
+    var body: some View {
+        Button ("Show sheet") {
+            self.showingSheet.toggle()
+            
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "jina")
+            
+        }
+
     }
 }
 

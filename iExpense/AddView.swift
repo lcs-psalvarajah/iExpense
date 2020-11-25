6a8bf6857a182cbd35faeceaf7b3e7d041eb7c75
jina <psalvarajah@lcs.on.ca>
2020-11-25 10:56:53 -0500

    Added a sheet called Addview, and then I linked it to the content view using a button

---
+++ iExpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Salvarajah, Prajina on 2020-11-25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name" , text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+        }
+    }
+}
+
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
